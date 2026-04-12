b774413f06d6b4fd79b6cdc932ec4eb17bb02545
Rocio <rtlopez@outlook.es>
2026-04-12 16:35:54 +0200

    869ct2aje #Complete

--- app/src/main/java/com/ro/macrotracker/utils/Extensions.kt
+++ app/src/main/java/com/ro/macrotracker/utils/Extensions.kt
@@ -1,7 +1,9 @@
 package com.ro.macrotracker.utils
 
-import kotlin.math.roundToInt
+import java.util.Locale
 
-    fun Double.format(): String {
-        return this.roundToInt().toString()
+    fun Double.format(decimals: Int = 1): String {
+        if (this == 0.0) return "0"
+        val format = "%.${decimals}f".format(Locale.ENGLISH, this)
+        return format.replace(".0", "").replace(",0", "")
     }
